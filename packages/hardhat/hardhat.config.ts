7570ceadbd77f2bba45ab3b0bb02b7702bf5692a
zvolsdan <danielzvolsky350@gmail.com>
2025-10-17 20:41:50 +0200

    change config for sepolia fork

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -28,10 +28,9 @@ const config: HardhatUserConfig = {
     // View the networks that are pre-configured.
     // If the network you are looking for is not here you can add new network settings
     hardhat: {
-      type: "edr-simulated",
+      type: "edr-simulated" as const,
       forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/${providerApiKey}`,
-        enabled: process.env.MAINNET_FORKING_ENABLED === "true",
+        url: `https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`,
       },
     },
     mainnet: {
