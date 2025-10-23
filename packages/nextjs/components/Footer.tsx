40b113a6315cba94fe4e445991ac4a452b98926e
zvolsdan <danielzvolsky350@gmail.com>
2025-10-23 19:19:48 +0200

    change footer color

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -5,7 +5,7 @@ import React from "react";
  */
 export const Footer = () => {
   return (
-    <div className="min-h-0 py-5 px-1 mb-11 lg:mb-0 bg-base-200 border-t border-base-300">
+    <div className="min-h-0 py-5 px-1 mb-11 lg:mb-0 bg-slate-800 border-t border-base-content/20">
       <div className="w-full">
         <div className="flex justify-center items-center text-sm w-full">
           <p className="m-0 text-center text-base-content/70">made by CTU Blockchain Lab</p>
